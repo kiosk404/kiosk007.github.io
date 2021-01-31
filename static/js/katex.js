d1dc41bdbda779b0bace8cf7dfd81a1554253d66
orion <weijiaxiang007@foxmail.com>
2021-01-31 22:30:37 +0800

    Site updated: 2021-01-31 22:30:36

---
+++ static/js/katex.js
@@ -0,0 +1,3 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>$(function () {
+    $('span.katex-display').wrap('<div class="katex-wrap"></div>')
+})
